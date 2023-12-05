bb3fc345ce9fb25a6e4bde8639165783258f765e
Rodgers Gitau <rodgersgitau@gmail.com>
2023-12-05 22:51:48 +0300

    chore: init svelte project

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./styles/styles.scss";
+
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app") as HTMLDivElement,
+});
+
+export default app;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Heading from "$lib/components/Heading.svelte";
+</script>
+
+<main>
+  <Heading>CV Match</Heading>
+</main>
